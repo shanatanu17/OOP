876d53e134819b86046b88d1c268a35ce0adf491
shantanu17 <shantanushinde3292@gmail.com>
2025-10-21 08:50:56 +0530

    first commit

---
+++ checkimplicit.java
@@ -0,0 +1,26 @@
+class create
+{
+ int a;
+
+ void display()
+ {
+  System.out.println(a);
+ }
+
+}
+
+
+
+public class checkimplicit
+{
+ public static void main(String ss[])
+ { 
+ 
+   create obj = new create();
+   obj.display();
+ 
+ 
+ }
+
+
+}
